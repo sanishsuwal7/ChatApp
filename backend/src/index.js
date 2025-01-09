95ac06a7113a5a8405a4d009e11ddb67c710866d
Sanish Suwal <sanishsuwal3@gmail.com>
2025-01-09 11:53:01 -0500

    changes for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -14,7 +15,8 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 // const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +27,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/vite_project/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/vite-project", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT,  () => {
     console.log("server is running on port " + PORT);
--- frontend/vite-project/src/store/useAuthStore.js
+++ frontend/vite-project/src/store/useAuthStore.js
@@ -3,6 +3,8 @@ import {axiosInstance} from "../lib/axios.js"
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "api";
+
 export const useAuthStore = create((set, get) => ({
     authUser:null,
     isSigninigUp:false,
@@ -87,7 +89,7 @@ export const useAuthStore = create((set, get) => ({
 
         if (!authUser || get().socket?.connected) return;
 
-        const socket = io('http://localhost:5001',{
+        const socket = io(BASE_URL,{
             query:{
                 userId: authUser._id,
             }

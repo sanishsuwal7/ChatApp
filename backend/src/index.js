a4bec70c957a830b8bdbbb4e2e01a742b522da67
Sanish Suwal <sanishsuwal3@gmail.com>
2025-01-09 12:10:02 -0500

    made some fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/vite_project/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/vite-project/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/vite-project", "dist", "index.html"));
